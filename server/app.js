1a4ae9458259e63c8592f23e7e91b007fe30e6f2
xenobarbital <greencross.am@gmail.com>
2019-11-14 14:14:22 +0400

    Add CORS

--- server/app.js
+++ server/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+// allow cross origin request
+app.use(cors());
 
 mongoose.connect(
   "mongodb+srv://xenobarbital:baberbekule@cluster0-1id95.mongodb.net/test?retryWrites=true&w=majority"
@@ -9,8 +15,6 @@ mongoose.connect(
 mongoose.connection.once('open', () => {
   console.log('Connected to DB');
 })
-
-const app = express();
 
 app.use('/graphql', graphqlHTTP({
   schema,
